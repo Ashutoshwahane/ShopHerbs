a64c7201e5c049dbaf7b8fd57b01a279881df349
Ashutoshwahane <ashutosh.wahane10@gmail.com>
2020-06-29 22:15:53 +0530

    add edit text

--- app/src/main/java/com/omkar/shopherb/activity/ui/shop/ShopFragment.java
+++ app/src/main/java/com/omkar/shopherb/activity/ui/shop/ShopFragment.java
@@ -4,18 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.omkar.shopherb.R;
 
 
 public class ShopFragment extends Fragment {
+
+    EditText symptoms;
 
     private ShopViewModel galleryViewModel;
 
@@ -24,13 +24,13 @@ public class ShopFragment extends Fragment {
         galleryViewModel =
                 ViewModelProviders.of(this).get(ShopViewModel.class);
         View root = inflater.inflate(R.layout.fragment_shop, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        symptoms = root.findViewById(R.id.searchShopEditText);
+        String symptomsText = symptoms.getText().toString();
+
+
+
+
         return root;
     }
 }

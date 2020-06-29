f4e76dcd262fd6d451f6c55fd6ceb9b92830ddad
Ashutoshwahane <ashutosh.wahane10@gmail.com>
2020-06-29 22:15:30 +0530

    intent not successful

--- app/src/main/java/com/omkar/shopherb/activity/ui/home/HomeFragment.java
+++ app/src/main/java/com/omkar/shopherb/activity/ui/home/HomeFragment.java
@@ -4,15 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.omkar.shopherb.R;
+import com.omkar.shopherb.activity.MainActivity;
+import com.omkar.shopherb.activity.ui.HospitalFragment;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
+    Button hospitalButton;
     private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -20,13 +24,29 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-//        final TextView textView = root.findViewById(R.id.text_home);
-//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
+
+        hospitalButton = root.findViewById(R.id.hospital_button);
+        hospitalButton.setOnClickListener(this);
+
+
+
+
+
+
+
         return root;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.hospital_button : {
+                HospitalFragment hospitalFragment = new HospitalFragment();
+                MainActivity mainActivity = (MainActivity) getActivity();
+                getFragmentManager().beginTransaction().replace(R.id.homeframelayout,hospitalFragment).commit();
+
+            }
+        }
+
     }
 }

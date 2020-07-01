e6072b7674e184e45080fd5f6d0e53d6d0aef05b
Ashutoshwahane <ashutosh.wahane10@gmail.com>
2020-07-01 23:28:32 +0530

    add clicklistener to change to layout

--- app/src/main/java/com/omkar/shopherb/activity/ui/shop/ShopFragment.java
+++ app/src/main/java/com/omkar/shopherb/activity/ui/shop/ShopFragment.java
@@ -1,21 +1,27 @@
 package com.omkar.shopherb.activity.ui.shop;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.omkar.shopherb.R;
+import com.omkar.shopherb.activity.fragment.FeverFragment;
 
 
 public class ShopFragment extends Fragment {
 
     EditText symptoms;
+    Button search;
+    String symptomsText = "";
 
     private ShopViewModel galleryViewModel;
 
@@ -26,11 +32,29 @@ public class ShopFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_shop, container, false);
 
         symptoms = root.findViewById(R.id.searchShopEditText);
-        String symptomsText = symptoms.getText().toString();
+        symptomsText = symptoms.getText().toString();
+
+        search = root.findViewById(R.id.searchShopButton);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShowFragment();
+            }
+        });
+
+
+
 
 
 
 
         return root;
     }
+
+    private void ShowFragment() {
+        FeverFragment feverFragment = new FeverFragment();
+        getChildFragmentManager().beginTransaction().replace(R.id.searchShopFrameLayout,feverFragment).commit();
+
+    }
+
 }

a8783a9fb398208475fb661cb2d83598668c5a44
Ashutoshwahane <ashutosh.wahane10@gmail.com>
2020-07-03 22:47:50 +0530

    TODO incompelete

--- app/src/main/java/com/omkar/shopherb/activity/ui/order/OrderFragment.java
+++ app/src/main/java/com/omkar/shopherb/activity/ui/order/OrderFragment.java
@@ -4,33 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.omkar.shopherb.R;
 
 
 public class OrderFragment extends Fragment {
 
-    private OrderViewModel slideshowViewModel;
-
+    Button click;
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                ViewModelProviders.of(this).get(OrderViewModel.class);
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_order, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        click = root.findViewById(R.id.clickme);
+        click.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+
             }
         });
+
+
         return root;
     }
 }
